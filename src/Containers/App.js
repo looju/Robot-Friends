4872afac1e9fe5542a4eccc80cd79cb409d2eaa1
omofade oluwaloju <omofade2019@gmail.com>
2022-08-19 03:55:39 +0100

    updated reeact pakages and used react hooks

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,64 +1,50 @@
-import React, { Component } from 'react';
+import React, {useState,useEffect } from 'react';
 import {Cardlist} from '../Components/Cardlist';
 import Searchbox from '../Components/Searchbox';
 import Scroll from '../Components/scroll';
 import ErrorBoundary from '../Components/ErrorBoundary';
 
+const App=()=>{
+    const[robots,setRobots]=useState([])
+    const[searchfield,setSearchfield]=useState('')
 
-import './App.css';
-class App extends  Component {
-constructor(){
-  super()
-  this.state={
-    robots:[ ],
-    searchfield:''
+ useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>{response.json()})
+    .then((user)=>{setRobots(user)})
+},[]) 
+const onSearch=(event)=>{
+ setSearchfield(event.target.value);
+}
+const filterBots=robots.filter((robots)=>{
+    return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+  })
+  console.log(robots,searchfield);
+  // console.log(filterBots)
+  if(!robots.length){
+   return <h1>Please wait...</h1>
   }
-} //It's the syntax for calling the state you have defined. 
-//A state is usually created between the components that wish to interact
-//State is an object that describes your application. They are usually used in the parent component which passes state to the children
-
-componentDidMount(){ //this is called after render in the react lifecycle. But since it affects the state and changes the DOM, render is called after it again to repaint the virtual DOM 
-console.log('heyyy')
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(response=>response.json())
-
-.then((users)=>{this.setState({robots:users})})
-}
-
-onSearch = (event) => {
- 
-  this.setState({searchfield: event.target.value});// this is so that the searchfield's original empty string is equivalent to the value of the event(which would be the value from the user input). We can't use something like this.state.searchfield. This.setstate is the standard
-}
-//onSearch=()=>{}. This syntax is only used when we are creating a method of our own. This is so that 'this' refers to the state defined by the parent 
-//this.state.robots.filter simply runs a function with robots(as parameter) to return the name of the robots in lowercase if the searchfield value tallies the robot name
-//remember that searchfield has been specified to be the value of the event
-//includes() is used to search strings for a particular substring and returns true or false depending on if the string is found
-  render(){
-    const filterBots=this.state.robots.filter((robots)=>{
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    })
-    // console.log(filterBots)
-    if(!this.state.robots){
-      <h1>Please wait...</h1>
-    }
-    else{
-      return(
-      
-        <div className='tc'>
-          <h1 className='f1'>ROBOTFRIENDS</h1> 
-          <Searchbox  onSearch={this.onSearch}/>
-          <Scroll>
-            <ErrorBoundary>
-            <Cardlist  robots={filterBots}/> 
-            </ErrorBoundary> 
-          </Scroll>
-         
-        </div>
-       
-         )
-    }
+  else{
+    return(
     
+      <div className='tc'>
+        <h1 className='f1'>ROBOTFRIENDS</h1> 
+        <Searchbox  onSearch={onSearch}/>
+        <Scroll>
+          <ErrorBoundary>
+          <Cardlist  robots={filterBots}/> 
+          </ErrorBoundary> 
+        </Scroll>
+       
+      </div>
+     
+       )
   }
+  
 }
-//searchbox now has a property which is the onsearch function.
+
 export default App
+
+
+
+
